2a262800e29e41bf8e94d8dc71fa9ffa70fafa26
Vicente Helano <vicentehelano@gmail.com>
2008-11-08 18:19:54 +0000

    Added the CoppeTeX homepage.

--- build_packages.sh
+++ build_packages.sh
@@ -1,5 +1,30 @@
 #!/bin/sh
 #
+# This is file `build_packages.sh'
+#
+# Automatically generates CoppeTeX packages for a specific release.
+#
+# Copyright (C) 2008 CoppeTeX Project and any individual authors listed
+# elsewhere in this file.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License version 3 for more details.
+#
+# You should have received a copy of the GNU General Public License
+# version 3 along with this package (see COPYING file).
+# If not, see <http://www.gnu.org/licenses/>.
+#
+# $URL$
+# $Id$
+#
+# Author(s): Vicente Helano <helano@inbox.com>
+#
 # TODO
 #   - automatic update downloads page
 #
