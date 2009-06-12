7c8152f017de803de94e3225797927f97a45b8f9
Vicente Helano <vicentehelano@gmail.com>
2009-06-12 23:02:30 +0000

    Started new homepage design.

--- htdocs/scripts/lastmod.js
+++ htdocs/scripts/lastmod.js
@@ -1,14 +1,12 @@
 function last_modified(lastchanged, lastauthor)
 {
-//  var lastchanged = "$LastChangedDate: 2009-03-05 20:47:07 -0300 (Thu, 05 Mar 2009) $"
-//  var lastauthor = "$LastChangedBy: helano $"
   lastchanged = lastchanged.replace(/.*\(/, "").replace(/\).*/, "");
   lastauthor = lastauthor.replace(/.*:\s/g, "").replace(/\s*.\$/, "");
   lastauthor = lastauthor[0].toUpperCase() + lastauthor.substr(1);
   document.write("Last modified on " + lastchanged + " by " + lastauthor);
 }
 
-function copyright_info()
+function show_copyright_info()
 {
   var copyright = new Date();
   document.write("<br>&copy; " + copyright.getFullYear() + " CoppeTeX Project");
