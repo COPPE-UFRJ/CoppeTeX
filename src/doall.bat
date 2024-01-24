a30ac67ce6e7cce25814b60806bcf8609e1e979d
Geraldo Xexéo <xexeo@cos.ufrj.br>
2024-01-24 18:18:05 -0300

    Deleting non useful dist, fixing abbrev

---
+++ src/doall.bat
@@ -0,0 +1,16 @@
+pdflatex coppe.ins
+pdflatex coppe.dtx
+makeindex -s gglo.ist -o coppe.gls coppe.glo
+makeindex -s gind.ist -o coppe.ind coppe.idx
+pdflatex coppe.dtx
+pdflatex example.tex
+makeindex -s gglo.ist -o coppe.gls coppe.glo
+makeindex -s gind.ist -o coppe.ind coppe.idx
+makeindex -s coppe.ist -o example.lab example.abx
+makeindex -s coppe.ist -o example.los example.syx
+pdflatex coppe.dtx
+pdflatex example.tex
+bibtex coppe
+bibtex example
+pdflatex example.tex
+
